bd13ce7cb0046f3192dae21a6765cb172626aa72
Bogdan <dbogdangabriel@gmail.com>
2022-08-09 15:57:34 +0300

    ApiFundamentals-5.0
    Tutorial for API

---
+++ CoreApiFundamentals-5.0/src/Data/CampProfile.cs
@@ -0,0 +1,18 @@
+﻿using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile: Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>();
+        }
+    }
+}
