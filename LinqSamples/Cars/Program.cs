b75d14842f6f64b94d65513564505ff14a367257
Bogdan <dbogdangabriel@gmail.com>
2022-08-17 17:17:27 +0300

    UPDATE
    Last LINQ update :+1:

--- LinqSamples/Cars/Program.cs
+++ LinqSamples/Cars/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
@@ -13,8 +14,70 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            CreateXML();
-            QueryXML();
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarDb>());
+            InsertData();
+            QueryData();
+            // CreateXML();
+            //  QueryXML();
+        }
+
+        private static void QueryData()
+        {
+            var db = new CarDb();
+            db.Database.Log = Console.WriteLine;
+
+            /*            var query = from car in db.Cars
+                                    orderby car.Combined descending, car.Name ascending
+                                    select car; 
+                  foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Name}: {car.Combined}");
+            }*/
+            /*
+                        var query =
+                            db.Cars.GroupBy(c => c.Manufacturer)
+                                .Select(g => new
+                                {
+                                    Name = g.Key,
+                                    Cars = g.OrderByDescending(c => c.Combined).Take(2)
+                                });*/
+
+            var query =
+                from car in db.Cars
+                group car by car.Manufacturer into manufacturer
+                select new
+                {
+                    Name = manufacturer.Key,
+                    Cars = (from car in manufacturer
+                           orderby car.Combined descending
+                           select car).Take(2)
+                };
+
+            foreach (var group in query)
+            {
+                Console.WriteLine(group.Name);
+                foreach (var car in group.Cars)
+                {
+                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
+                }
+
+            }
+
+        }
+
+        private static void InsertData()
+        {
+            var cars = ProcessCars("fuel.csv");
+            var db = new CarDb();
+            
+            if (!db.Cars.Any())
+            {
+                foreach (var car in cars)
+                {
+                    db.Cars.Add(car);
+                }
+                db.SaveChanges();
+            }
         }
 
         private static void QueryXML()
